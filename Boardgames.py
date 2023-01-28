b24c7fb37ef01f3d008d3c66773b93364905ad3c
Samy Chaabi <xhackax47@gmail.com>
2023-01-28 12:29:16 +0100

    Visualisation graphique des stats de données

--- Boardgames.py
+++ Boardgames.py
@@ -1,4 +1,5 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # -*- coding: utf-8 -*-
 """
@@ -7,11 +8,14 @@ Created on Sat Jan 28 12:13:23 2023
 @author: samy_
 """
 
-# Importer la bibliothèque pandas.
-import pandas
 # Lire les données.
-games = pandas.read_csv("games.csv")
+games = pd.read_csv("datasets/games.csv")
+
 # Afficher les noms de colonnes du DataFrame games.
 print("Colonne : " + games.columns)
 # Afficher les dimensions du Dataframe ex: 81312 lignes et 20 colonnes)
 print("Dimensions : " + str(games.shape))
+# Histogramme de toutes les notes de la colonne average_rating.
+plt.hist(games["average_rating"])
+# Afficher le graphique
+plt.show()
